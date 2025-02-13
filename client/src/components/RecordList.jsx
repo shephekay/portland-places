e5bb02f922e70380e34f3b97971add0a388d9e9d
Sheila Kelley <sheilaphedrakelley@gmail.com>
2025-02-13 15:16:29 -0800

    tailwind working correctly

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -21,7 +21,7 @@ const Record = (props) => (
           Edit
         </Link>
         <button
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background text-gray-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
           color="red"
           type="button"
           onClick={() => {
